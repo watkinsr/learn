c431e99eefca97d542b9b14d0ed322842826c994
Ryan Watkins <ryanwatkins54@gmail.com>
2018-09-11 21:03:54 +0000

    mod: add more functions test answers

--- src/learn/core.clj
+++ src/learn/core.clj
@@ -22,3 +22,21 @@
 (assert (= "Hello, World!" (greeting)))
 (assert (= "Hello, Clojure!" (greeting "Clojure")))
 (assert (= "Good morning, Clojure!" (greeting "Good morning" "Clojure")))
+
+(defn do-nothing [x] x)
+
+(source identity)
+
+(defn always-thing [x & rest] :thing)
+
+(defn make-thingy [x & rest] (fn [& args] x))
+
+;; Tests
+(let [n (rand-int Integer/MAX_VALUE)
+      f (make-thingy n)]
+  (assert (= n (f)))
+  (assert (= n (f :foo)))
+  (assert (= n (apply f :foo (range)))))
+
+(defn triplicate [f] (do (f)(f)(f)))
+(triplicate (fn [] (println "Hello")))
